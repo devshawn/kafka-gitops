061110432992874bbdef740bd1beecacb167907d
Shawn Seymour <shawn@devshawn.com>
2020-04-10 15:12:32 -0500

    release: 0.2.1!

--- src/main/java/com/devshawn/kafka/gitops/MainCommand.java
+++ src/main/java/com/devshawn/kafka/gitops/MainCommand.java
@@ -12,7 +12,7 @@ import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "kafka-gitops",
-        version = "0.2.0",
+        version = "0.2.1",
         exitCodeOnInvalidInput = 0,
         subcommands = {
                 AccountCommand.class,
